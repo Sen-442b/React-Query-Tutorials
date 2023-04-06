13267f3343df1c196e984fcbc5a1257285072198
shubhendu.sen@homepageit.net <tushar.sharma18@outlook.com>
2023-04-06 17:29:23 +0530

    pollign

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -6,7 +6,7 @@ const fetchSuperHeroes= ()=>{
   return axios.get("http://localhost:4000/superheroes")
 }
 export const RQSuperHeroesPage = () => {
- const results= useQuery("super-heros",fetchSuperHeroes)
+ const results= useQuery("super-heros",fetchSuperHeroes,{refetchInterval:2000,refetchIntervalInBackground:true})
  const {isLoading,data, isError,error,isFetching}=results
   console.log({isFetching, isLoading})
  if(isLoading){
